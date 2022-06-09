caee1762dd6990b1d34129a36458106cef94bccb
Ayanswamyyadav <ayanswamyyadav@gmail.com>
2022-06-09 15:24:30 +0530

    frist commit

---
+++ src/test/java/w3schools/RestAssured/Single.java
@@ -0,0 +1,19 @@
+package w3schools.RestAssured;
+
+public class Single {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("sekhar");
+		System.out.println("mahesh");
+		System.out.println("govind");
+		System.out.println("prashatha");
+		System.out.println("ganesh");
+		
+
+
+	}
+
+}
